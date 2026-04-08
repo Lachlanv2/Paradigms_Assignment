de0692dfcfa9c2e7357b78e8348349df1e937d42
Lachlanv2 <lochy.marshall@protonmail.com>
2026-04-08 17:43:20 +0800

    testing with While loops

--- src/main.java
+++ src/main.java
@@ -1,6 +1,14 @@
+import java.util.Scanner; //https://www.tutorialspoint.com/java/java_user_input.html
 public class main {
   public static void main(String[] args) {
-    System.out.println("Hello Universe!!!!!!!!");
-
+    System.out.println("Welcome to the Lanugage Guessing Game");
+    System.out.println("This test will present you with the words HELLO WORLD in another language alongside weather info and currency");
+    System.out.println("You have to guess the country of origin");
+    while(true) { //Main loop
+      Scanner obj = new Scanner(System.in);
+      System.out.println("What is your username");
+      String test = obj.nextLine();
+      System.out.println("you put in: " + test);
+    }
   }
 }
