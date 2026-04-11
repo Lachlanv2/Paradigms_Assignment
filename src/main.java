b926c81062995ec6fbc24854f1f4085926a91788
Lachlanv2 <lochy.marshall@protonmail.com>
2026-04-11 16:38:41 +0800

    Getting ready for API call

--- src/main.java
+++ src/main.java
@@ -1,14 +1,24 @@
 import java.util.Scanner; //https://www.tutorialspoint.com/java/java_user_input.html
+import java.net.http.HttpRequest;
+//import java.util.Thread;
 public class main {
-  public static void main(String[] args) {
-    System.out.println("Welcome to the Lanugage Guessing Game");
+  public static void main(String[] args) throws InterruptedException {
+    System.out.println("Welcome to the Language Guessing Game");
     System.out.println("This test will present you with the words HELLO WORLD in another language alongside weather info and currency");
     System.out.println("You have to guess the country of origin");
+    Thread.sleep(3000);
+    Scanner obj = new Scanner(System.in);
+    System.out.println("\nWhat is your username");
+    String playername = obj.nextLine();
+
+
+
+
     while(true) { //Main loop
-      Scanner obj = new Scanner(System.in);
-      System.out.println("What is your username");
-      String test = obj.nextLine();
-      System.out.println("you put in: " + test);
+      int questions = 0;
+      while(questions != 10);
+        System.out.println("Question" + questions);
+
     }
   }
 }
